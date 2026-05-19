09c6e4e6326510545ec7edcf2342d8f99e01eecd
Zanda Kumsha <zandaj.kumsha@gmail.com>
2026-05-19 12:45:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Variables
+const firstName = "Zanda";
+const lastName = "Kumsha";
+let age = 18;
+
+const fullName = firstName + " " + lastName;
+
+console.log(`Hello ${fullName} , your age is :${age}`);
+
+age = 22;
+console.log(`Hello ${fullName} , your new age is :${age}`);
